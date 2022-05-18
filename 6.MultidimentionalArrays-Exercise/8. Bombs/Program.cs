55ac98ad5182b59156fb421a1d50555028c64a50
evgeni136 <evgeni_136@abv.bg>
2022-05-18 13:41:16 +0300

    matrix exercises commit

---
+++ 6.MultidimentionalArrays-Exercise/8. Bombs/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int[] coordinates = Console.ReadLine().Split( new char[] {',', ' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            for (int i = 0; i < coordinates.Length; i += 2)
+            {
+                int row = coordinates[i];
+                int col = coordinates[i + 1];
+
+                if (matrix[row, col] <= 0)
+                {
+                    continue;
+                }
+
+                //up
+                if (IsInRange(matrix, row - 1, col) && matrix[row - 1, col] > 0)
+                {
+                    matrix[row - 1, col] -= matrix[row, col];
+                }
+
+                //down
+                if (IsInRange(matrix, row + 1, col) && matrix[row + 1, col] > 0)
+                {
+                    matrix[row + 1, col] -= matrix[row, col];
+                }
+                //Left
+                if (IsInRange(matrix, row, col - 1) && matrix[row, col - 1] > 0)
+                {
+                    matrix[row, col - 1] -= matrix[row, col];
+                }
+                //right
+                if (IsInRange(matrix, row, col + 1) && matrix[row, col + 1] > 0)
+                {
+                    matrix[row, col + 1] -= matrix[row, col];
+                }
+                //upLeft
+                if (IsInRange(matrix, row - 1, col - 1) && matrix[row - 1, col - 1] > 0)
+                {
+                    matrix[row - 1, col - 1] -= matrix[row, col];
+                }
+                //upRight
+                if (IsInRange(matrix, row - 1, col + 1) && matrix[row - 1, col + 1] > 0)
+                {
+                    matrix[row - 1, col + 1] -= matrix[row, col];
+                }
+                //downLeft
+                if (IsInRange(matrix, row + 1, col - 1) && matrix[row + 1, col - 1] > 0)
+                {
+                    matrix[row + 1, col - 1] -= matrix[row, col];
+                }
+                //downRight
+                if (IsInRange(matrix, row + 1, col + 1) && matrix[row + 1, col + 1] > 0)
+                {
+                    matrix[row + 1, col + 1] -= matrix[row, col];
+                }
+
+                matrix[row, col] = 0;
+            }
+
+            int aliveCellsCounter = 0;
+            int sumAliveCells = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveCellsCounter++;
+                        sumAliveCells += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCellsCounter}");
+            Console.WriteLine($"Sum: {sumAliveCells}");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool IsInRange(int[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) &&
+                col >= 0 && col < matrix.GetLength(1);
+        }
+    }
+}
