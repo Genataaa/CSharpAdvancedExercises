a03431e6d65e1e749afe49acbc6196d6af586c3f
evgeni136 <evgeni_136@abv.bg>
2022-05-22 19:21:15 +0300

    Count Symbols Task

---
+++ 8.Sets and Dictionaries Advanced-Exercise/5. Count Symbols/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            SortedDictionary<char, int> results = new SortedDictionary<char, int>();
+            foreach (var character in charArray)
+            {
+                if (!results.ContainsKey(character))
+                {
+                    results.Add(character, 0);
+                }
+                results[character]++;
+            }
+
+            foreach (var result in results)
+            {
+                Console.WriteLine($"{result.Key}: {result.Value} time/s");
+            }
+        }
+    }
+}
